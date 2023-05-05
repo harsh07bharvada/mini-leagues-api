5cc0aca57857746dcac5fb82122d5d645823c548
Harsh Bharvada <harsh07bharvada@gmail.com>
2023-05-05 14:57:35 +0530

    Mini League API - Add user agent in the header of axios request

--- src/utils/fetch.util.ts
+++ src/utils/fetch.util.ts
@@ -1,40 +1,40 @@
-import axios, { AxiosResponse } from 'axios';
+import axios, { AxiosResponse } from 'axios'
 export default class HTTPService {
-
-    /**
-     * 
-     * @param url 
-     * @param data 
-     * @returns 
-     */
-    static async post(url: string, data: any) {
-        try {
-            let response: AxiosResponse = await axios.post(url, {
-                method: "POST",
-                body: data,
-            })
-            return response.data;
-        }
-        catch (err) {
-            throw err
-        }
+  /**
+   *
+   * @param url
+   * @param data
+   * @returns
+   */
+  static async post(url: string, data: any) {
+    try {
+      let response: AxiosResponse = await axios.post(url, {
+        method: 'POST',
+        body: data,
+      })
+      return response.data
+    } catch (err) {
+      throw err
     }
+  }
 
-    /**
-     * 
-     * @param url 
-     * @param data 
-     * @returns 
-     */
-    static async get(url: string) {
-        try {
-            let response: AxiosResponse = await axios.get(url, {
-                method: "GET"
-            })
-            return response.data;
-        }
-        catch (err) {
-            throw err
-        }
+  /**
+   *
+   * @param url
+   * @param data
+   * @returns
+   */
+  static async get(url: string) {
+    try {
+      let response: AxiosResponse = await axios.get(url, {
+        method: 'GET',
+        headers: {
+          'User-Agent': 'mini-leagues-api',
+        },
+      })
+      return response.data
+    } catch (err) {
+      throw err
     }
+  }
 }
